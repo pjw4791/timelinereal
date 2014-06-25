6b4b1dcc5df77f0e06a70c97ff1cfb0391166979
SeoYoung Park <apple@apple-ui-MacBook-Air.local>
2014-06-25 10:47:27 +0900

    타임라인 글 삭제 페이지 만들기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -30,7 +30,12 @@ class WallController < ApplicationController
       redirect_to :back
     end
   end
+  
   def posts
     @posts=Post.all
+  end
+
+  def delete
+    @post_delete=Post.find(params[:id])
   end
 end

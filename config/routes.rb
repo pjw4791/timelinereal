8e459888bee14a578e4a3ea6cfa72bc9eacf8626
SeoYoung Park <apple@apple-ui-MacBook-Air.local>
2014-06-25 10:35:28 +0900

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Timeline::Application.routes.draw do
   get "wall/posts" => 'wall#posts'
   post "wall/write_complete"
   get "wall/edit/:id"=>'wall#edit'
-  # The priority is based upon order of creation: first created -> highest priority.
+  post "wall/edit_complete"
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
